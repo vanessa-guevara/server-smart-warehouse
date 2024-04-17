229bfb15cbbec0f30b5fa43845b80a92e280a8f5
vanessa-guevara <155489832+vane17a@users.noreply.github.com>
2024-04-16 23:24:03 -0700

    install bcryptjs

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,20 @@
-require('dotenv').config()
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports= {
+require('dotenv').config();
+
+const dbConfig = process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+
+module.exports = {
   client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
+  connection: dbConfig,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
 };
